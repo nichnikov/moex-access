4a813e75e8c4d8efc8a99fc72e8ef926291d0499
nichnikov <a.nichnikov@gmail.com>
2024-09-06 22:59:15 +0300

    ref

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # moex-access
+
 доступ к API московской биржи
 
 https://habr.com/ru/articles/677072/ 
 
 https://wlm1ke.github.io/apimoex/build/html/getting_started.html#id2
+
+
+
+Временные ряды:
+
+https://habr.com/ru/companies/ods/articles/327242/
+
+https://habr.com/ru/articles/553658/
--- sberp_analysis.py
+++ sberp_analysis.py
@@ -14,11 +14,56 @@ def exponential_smoothing(series, alpha):
         result.append(alpha * series[n] + (1 - alpha) * result[n-1])
     return result
 
-df = pd.read_csv(os.path.join("data", "sberp.csv"))
-print(df)
+def plotMovingAverage(series, n):
 
+    """
+    series - dataframe with timeseries
+    n - rolling window size 
 
+    """
 
-plt.figure(figsize=(16,10), dpi= 80)
-plt.plot('TRADEDATE', 'CLOSE', data=df, color='tab:red')
-plt.show()
+    rolling_mean = series.rolling(window=n).mean()
+
+    # При желании, можно строить и доверительные интервалы для сглаженных значений
+    #rolling_std =  series.rolling(window=n).std()
+    #upper_bond = rolling_mean+1.96*rolling_std
+    #lower_bond = rolling_mean-1.96*rolling_std
+
+    plt.figure(figsize=(15,5))
+    plt.title("Moving average\n window size = {}".format(n))
+    plt.plot(rolling_mean, "g", label="Rolling mean trend")
+
+    #plt.plot(upper_bond, "r--", label="Upper Bond / Lower Bond")
+    #plt.plot(lower_bond, "r--")
+    plt.plot(dataset[n:], label="Actual values")
+    plt.legend(loc="upper left")
+    plt.grid(True)
+
+if __name__ == "__main__":
+
+    df = pd.read_csv(os.path.join("data", "sberp.csv"))
+    print(df)
+    df.fillna(0.0, inplace=True)
+    
+    '''
+    res = exponential_smoothing(df.CLOSE, 0.01)
+    print(res)
+    print(len(res))'''
+
+    with plt.style.context('seaborn-v0_8-whitegrid'):
+        alpha = 0.01
+        plt.figure(figsize=(20, 8))
+        for alpha in [0.1, 0.05, 0.3, 0.01]:
+            plt.plot(exponential_smoothing(df.CLOSE, alpha), label="Alpha {}".format(alpha))
+        plt.plot(df.CLOSE.values, "c", label = "Actual")
+        plt.legend(loc="best")
+        plt.axis('tight')
+        plt.title("Exponential Smoothing")
+        plt.grid(True)
+        plt.show()
+    
+    '''
+    plt.figure(figsize=(16,10), dpi= 80)
+    plt.plot('TRADEDATE', 'CLOSE', data=df, color='tab:red')
+    plt.show()
+    '''
