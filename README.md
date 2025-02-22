24ec91872ac45dfecdf35fe4af365a863b4a5784
nichnikov <a.nichnikov@gmail.com>
2025-02-22 14:10:40 +0300

    ref

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 https://habr.com/ru/articles/677072/ 
 
 https://wlm1ke.github.io/apimoex/build/html/getting_started.html#id2
+
+https://external.software/archives/12582
--- rep_history_stoks.py
+++ rep_history_stoks.py
@@ -1,14 +1,38 @@
+"""
+https://external.software/archives/12582
+"""
 import os
 import requests
 import pandas as pd
 from apimoex import get_market_candles
+import pyodbc
+
+def execute_query(conn: pyodbc.Connection, query: str) -> None:
+    """Выполняет SQL-запрос."""
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
 
 
-with requests.Session() as session:
-    data = get_market_candles(session=session, security="SBER", start="2023-05-06", end="2024-06-06", interval=60)
-    print(data)
-    data_df = pd.DataFrame(data)
+def connect_to_access_db(db_file: str) -> pyodbc.Connection:
+    """Устанавливает соединение с базой данных Access."""
+    connection_string = rf'DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={db_file};'
+    return pyodbc.connect(connection_string)
 
-print(data_df)
-data_df.to_csv(os.path.join("data", "sber_stocks_Y.csv"), index=False)
-    
+conn = connect_to_access_db(os.path.join('data', 'Finance.accdb'))
+
+
+securities = ["SBER", "SNGSP", "ROSN", "MGNT", "LKOH", "YDEX"]
+
+for security in securities:
+    execute_query(conn, "DELETE FROM securities_price WHERE security = '{}'".format(security))
+    with requests.Session() as session:
+        data = get_market_candles(session=session, security=security, start="2025-02-01", end="2025-02-21", interval=60)
+        data_df = pd.DataFrame(data)
+
+
+    for line in list(data_df.itertuples(index=False, name=None)):
+        line = (security, ) + line
+        q = 'INSERT INTO securities_price (security, begin, open, close, high, low, volume, quantity) VALUES {}'.format(line)
+        execute_query(conn, q)
